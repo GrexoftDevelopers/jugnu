65a8d946cd4b0dfeeb3ceae5bff8a35f982fbd33
Zushan <ahzushan@gmail.com>
2016-04-29 20:22:22 +0530

    some changes in destroyer.cs

--- Assets/Destroyer.cs
+++ Assets/Destroyer.cs
@@ -1,19 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Destroyer : MonoBehaviour {
+public class Destroyer : MonoBehaviour
+{
+    Vector3 cameposition;
+    Camera cam;
+    float height;
+    float width;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = Camera.main;
+        height = 2f * cam.orthographicSize;
+        width = height * cam.aspect;
+        transform.position = new Vector3(-(cameposition.x + (width / 2)), cameposition.y, 0);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameposition = GameObject.FindGameObjectWithTag("MainCamera").transform.position;
+
+    }
     public void OnCollisionEnter2D(Collision2D coll)
     {
-        Destroy(coll.gameObject);
+        if (coll.gameObject.tag == "Star")
+        {
+            GameObject star = (GameObject)Instantiate(Resources.Load("star_prefab"));
+
+            star.transform.position = new Vector3((cameposition.x + (width / 2) + Random.Range(1, 2)), Random.Range(2, -2), 0);
+            Destroy(coll.gameObject);
+
+
+        }
+        else
+        {
+            Destroy(coll.gameObject);
+        }
     }
 }
